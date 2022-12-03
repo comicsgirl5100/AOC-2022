d816013a1f2433906f6481433d4278b29ea235ee
comicsgirl5100 <haliehagerty@gmail.com>
2022-12-03 12:13:00 -0700

    Day 1
    Day 1 of Advent of Code 2022

---
+++ main.py
@@ -0,0 +1,26 @@
+#2022 Advent of Code Challenges
+
+#Day 1 Part 1
+with open("input_files/day01.txt", "r") as f:
+    data = f.read().split("\n")
+
+    cals = 0
+    totalCals = []
+    for line in data:
+        if line != "":
+            cals = cals + int(line)
+
+        else:
+            totalCals.append(cals)
+            cals = 0
+
+    print(max(totalCals))
+#Day 1 Part 2
+    totalCals.sort(reverse = True)
+    topCals = totalCals[:3]
+    total = sum(topCals)
+    print(total)
+
+
+
+
